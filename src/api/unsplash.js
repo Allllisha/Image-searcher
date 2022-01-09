2b29690cbcf5d128ffdeb125690879d1c8efbfd5
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2022-01-09 18:03:19 +0900

    Updates

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers:{
-    Authorization: 'Client-ID 1Drx0kMqr86FKVRLqeA3omXm_sU3HDNVBz83fGSewuw'
-  }
-});
-
